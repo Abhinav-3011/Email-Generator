53a044a8b7f6a03c737b9b53c7a8770d9e07a77a
Abhinav <abhinavkr991@gmail.com>
2024-09-03 02:08:22 +0530

    first commit

---
+++ src/EmailApp/EmailSec.java
@@ -0,0 +1,10 @@
+package EmailApp;
+
+public class EmailSec {
+
+    public static void main(String[] args) {
+        // Creating a new Email object
+        Email em1 = new Email("Abhinav", "Kumar");
+       System.out.println( em1.showInfo());  
+    }
+}
